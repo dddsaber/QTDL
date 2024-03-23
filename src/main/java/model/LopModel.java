fe513522bb44264204a8b1a3777fc02158c77efc
Cao Van Kien <kienb2105578@student.ctu.edu.vn>
2024-03-23 14:23:28 +0700

    class LopModel

--- src/main/java/model/LopModel.java
+++ src/main/java/model/LopModel.java
@@ -1,5 +1,65 @@
 package model;
 
-public class LopModel {
+import java.util.ArrayList;
 
+import dao.LopDAO;
+
+public class LopModel {
+    private ArrayList<Lop> dsLop;
+    private LopDAO lopDao;
+    
+
+    //contructor
+    public LopModel(ArrayList<Lop> dsLop, LopDAO lopDao) {
+        this.dsLop = dsLop;
+        this.lopDao = lopDao;
+    }
+    public LopModel(ArrayList<Lop> dsLop) {
+        this.dsLop = dsLop;
+    }
+    public LopModel() {
+        this.dsLop = new ArrayList<Lop>();
+        this.lopDao = new LopDAO();
+    }
+
+
+    
+    public ArrayList<Lop> getDsLop() {
+        return dsLop;
+    }
+    public void setDsLop(ArrayList<Lop> dsLop) {
+        this.dsLop = dsLop;
+    }
+    public LopDAO getLopDao() {
+        return lopDao;
+    }
+    public void setLopDao(LopDAO lopDao) {
+        this.lopDao = lopDao;
+    }
+
+    public void insert(Lop Lop) {
+		this.dsLop.add(Lop);
+		this.lopDao.saveOrUpdate(Lop);
+	}
+	
+	public void delete(Lop Lop) {
+		this.dsLop.remove(Lop);
+		this.lopDao.delete(Lop);
+	}
+	
+	public void update(Lop Lop) {
+		this.lopDao.saveOrUpdate(Lop);
+		this.dsLop.remove(Lop);
+		this.dsLop.add(Lop);
+	}
+	
+	public boolean daTonTai(Lop lop) {
+		for(Lop Lop : dsLop) {
+			if(Lop.getMaLop().equals(lop.getMaLop())) {
+				return true;
+			}
+		}
+		return false;
+	}
+    
 }
