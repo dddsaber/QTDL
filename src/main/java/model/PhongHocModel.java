126b9a9fc7c53c46e12bf2a90b22cf2a25a05991
Cao Van Kien <kienb2105578@student.ctu.edu.vn>
2024-03-23 14:25:51 +0700

    class PhongHocModel

--- src/main/java/model/PhongHocModel.java
+++ src/main/java/model/PhongHocModel.java
@@ -1,5 +1,63 @@
 package model;
 
-public class PhongHocModel {
+import java.util.ArrayList;
 
+import dao.PhongHocDAO;
+
+public class PhongHocModel {
+    private ArrayList<PhongHoc> dsPhongHoc;
+    private PhongHocDAO phongHocDAO;
+
+    
+    public PhongHocModel(ArrayList<PhongHoc> dsPhongHoc, PhongHocDAO phongHocDAO) {
+        this.dsPhongHoc = dsPhongHoc;
+        this.phongHocDAO = phongHocDAO;
+    }
+    public PhongHocModel(ArrayList<PhongHoc> dsPhongHoc) {
+        this.dsPhongHoc = dsPhongHoc;
+    }
+    public PhongHocModel() {
+        this.dsPhongHoc = new ArrayList<PhongHoc>();
+        this.phongHocDAO = new PhongHocDAO();
+    }  
+
+
+    public ArrayList<PhongHoc> getDsPhongHoc() {
+        return dsPhongHoc;
+    }
+    public void setDsPhongHoc(ArrayList<PhongHoc> dsPhongHoc) {
+        this.dsPhongHoc = dsPhongHoc;
+    }
+    public PhongHocDAO getPhongHocDAO() {
+        return phongHocDAO;
+    }
+    public void setPhongHocDAO(PhongHocDAO phongHocDAO) {
+        this.phongHocDAO = phongHocDAO;
+    }
+    public void insert(PhongHoc PhongHoc) {
+  		this.dsPhongHoc.add(PhongHoc);
+  		this.phongHocDAO.saveOrUpdate(PhongHoc);
+  	}
+  	
+  	public void delete(PhongHoc PhongHoc) {
+  		this.dsPhongHoc.remove(PhongHoc);
+  		this.phongHocDAO.delete(PhongHoc);
+  	}
+  	
+  	public void update(PhongHoc PhongHoc ) {
+  		this.phongHocDAO.saveOrUpdate(PhongHoc);
+  		this.dsPhongHoc.remove(PhongHoc);
+  		this.dsPhongHoc.add(PhongHoc);
+  	}
+  	
+  	public boolean daTonTai(PhongHoc ph) {
+  		for(PhongHoc PhongHoc: dsPhongHoc) {
+  			if(PhongHoc.getMaPhong().equals(ph.getMaPhong())) {
+  				return true;
+  			}
+  		}
+  		return false;
+  	}
+
+    
 }
