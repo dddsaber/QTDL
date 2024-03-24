ca5aa16bdc498872c0b867b5a162c09225b45d4e
dddsaber <nhbl.loc@gmail.com>
2024-03-24 14:28:56 +0700

    Add lop hoc listener

---
+++ src/main/java/controller/LopHocListener.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.QLHS;
+
+public class LopHocListener implements ActionListener {
+	private QLHS view;
+	
+	
+	public LopHocListener(QLHS view) {
+		this.view = view;
+	}
+
+
+	public void actionPerformed(ActionEvent e) {
+		String actionCommand = e.getActionCommand();
+		if(actionCommand.equals("Tạo mới")) {
+			view.xoaTextFieldLH();
+		}
+		else if(actionCommand.equals("Xoá")) {
+			view.xoaLopHoc();
+		}
+		else if(actionCommand.equals("Lưu")) {
+			view.luuDuLieuLHtuInput();
+		}
+		else if(actionCommand.equals("Chọn")) {
+			view.hienThiThongTinLopHocDangChon();
+		}
+		else if(actionCommand.equals("Huỷ tìm")) {
+			view.huytimLH();
+		}
+		else if(actionCommand.equals("Tìm kiếm")) {
+			view.timKiemLopHoc();
+		}
+
+}
